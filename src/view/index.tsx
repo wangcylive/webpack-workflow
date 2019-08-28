2902ffe1493d0a577bd4682b4370845bd66c4878
wangcylive <wangcylive@outlook.com>
2019-08-28 23:41:17 +0800

    feat: 热更新代码修改

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,6 +1,6 @@
+import { hot } from 'react-hot-loader/root'
 import React from 'react'
 import { Provider } from 'react-redux'
-import { hot } from 'react-hot-loader/root'
 import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom'
 import { routes } from '../router'
 import store from '../store'
