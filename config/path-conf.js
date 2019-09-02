6ee43d5b07a598832ab56cac6c71e4bdf7566cb9
wangcylive <wangcylive@outlook.com>
2019-09-03 01:30:17 +0800

    feat: webpack config update

--- config/path-conf.js
+++ config/path-conf.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const { isProduction } = require('./env-conf')
 
-console.log('path', process.env.NODE_ENV)
-
 const assetsPath = 'assets'
 
 function getAssetsPath (mode, pathname) {
