5d1407e38de01e786b04f5649c12d3f2ab9bcd8f
wangchunyang1 <wangchunyang1@yy.com>
2020-03-03 17:06:57 +0800

    feat: 增加 prettier 格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'prettier',
     'prettier/@typescript-eslint',
     'prettier/react',
     'plugin:jest/recommended',
